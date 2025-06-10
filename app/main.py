6d6879b561c13bf86f20149a8f8370626832054a
Mohd Kaif <kaifmohd5000@gmail.com>
2025-06-11 01:27:52 +0530

    fix: added url for deployed link in CORS

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Anilyst Backend", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://anilyst.tech","https://www.anilyst.tech"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
